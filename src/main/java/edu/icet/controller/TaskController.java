382d42f52455b89e9caa81dea7d3b49c9a163b59
bavindushan <bavindushan65@gmail.com>
2025-04-19 14:29:35 +0530

    completed TaskServices and TaskRepository

--- src/main/java/edu/icet/controller/TaskController.java
+++ src/main/java/edu/icet/controller/TaskController.java
@@ -1,13 +1,21 @@
 package edu.icet.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.icet.dto.Task;
+import edu.icet.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/task")
+@RequiredArgsConstructor
 public class TaskController {
 
-    private void addTask(){
+    final TaskService service;
 
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    private void addTask(@RequestBody Task task){
+        service.add(task);
     }
 }
---
+++ src/main/java/edu/icet/service/TaskService.java
@@ -0,0 +1,7 @@
+package edu.icet.service;
+
+import edu.icet.dto.Task;
+
+public interface TaskService {
+    public void add(Task task);
+}
